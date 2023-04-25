0186adddc741507999f70809180041812cdf0c3a
otto <otto@tower.io>
2023-04-25 13:31:09 -0400

    Remove extraneous code

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ require('dotenv').config();
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
-// console.log(CLIENT_ID);
-// console.log(CLIENT_SECRET);
-// console.log(ACCESS_TOKEN);
 
 const getToken = async () => {
   const options = {
