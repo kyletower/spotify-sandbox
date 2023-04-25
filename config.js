da3e9f20a46cd2c443db8646101ecba5a99325ca
otto <otto@tower.io>
2023-04-25 12:38:42 -0400

    Add config.js template

---
+++ config.js
@@ -0,0 +1,14 @@
+// Sample of a config.js
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+  version: '1.2.3',
+  canonical_url: process.env.APPLICATION_ROOT,
+  api: {
+    host: process.env.API_HOST,
+    key: process.env.API_KEY,
+    secret: process.env.API_SECRET,
+  },
+  plugins: ['plugin-one', 'plugin.two'],
+};
